ae6fe70e34fb30c339b01fc39149f97baa8ab1db
McFadden-S <shaefromhome@shaw.ca>
2018-01-11 09:01:46 -0600

    V1 Pre-Coding formatting has been done

---
+++ src/A3Client.java
@@ -0,0 +1,44 @@
+// **********************************************************************
+// Programmer:	McFadden
+// Class:	CS30S
+//
+// Assignment:	Assignment Three
+//
+// Description:	The program will read hours worked and wage from a file and 
+//              calculate the pay of the employee. calulations will take place
+//              in the class and printing in client
+//
+// Input:       Hours worked and wage will be read in
+//
+// Output:      Will out put employee id, wage, hours worked, regular pay
+//              overtime pay and gross pay
+// ***********************************************************************
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class A3Client
+{  // begin class
+	public static void main(String args[])
+	{  // begin main
+	// ***** declaration of constants *****
+	// ***** declaration of variables *****
+	// ***** create objects *****
+	// ***** create input stream *****	
+	// ***** Print Banner *****
+	
+		System.out.println("**********************************");
+		System.out.println("NAME:        McFadden");
+		System.out.println("Class:       CS30S");
+		System.out.println("Assignment:  Assignment Three");
+		System.out.println("**********************************");
+		
+	// ***** get input *****
+	// ***** processing *****
+	// ***** output *****
+	// ***** closing message *****
+	
+		System.out.println("end of processing");
+	
+	}  // end main	
+}  // end class
---
+++ src/Employee.java
@@ -0,0 +1,26 @@
+/* **********************************************************
+ * Programmer:	McFadden
+ * Class:       CS30S
+ * 
+ * Assignment:	Assignment Three
+ *
+ * Description:	Calculate employee's pay given their hourly wage and amount of
+ *              hours worked
+ * ************************************************************/
+ 
+ 
+ public class Employee
+ {  // begin class
+ 	
+ 	// *********** class constants **********
+ 	
+ 	
+ 	// ********** instance variable **********
+ 	
+ 	// ********** constructors ***********
+ 	
+ 	// ********** accessors **********
+ 	
+ 	// ********** mutators **********
+ 
+ }  // end class
